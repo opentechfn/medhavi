c0d76fbbe674c093a89e8f93f2b32d96dc3973ec
namrata <sitlani.namrata@yahoo.in>
2019-09-17 17:36:55 +0530

    ssh module using paramiko

---
+++ ssh/conf/ssh_conf.py
@@ -0,0 +1,9 @@
+HOST = "Enter your host details"
+USERNAME ="Enter your username"
+PASSWORD="Enter your password"
+#PASSWORD=""
+PORT = 22
+TIMEOUT = 10
+PKEY = "Enter your key filename here"
+#Sample commands to execute(Add your commands here)
+COMMANDS = ['ls;mkdir sample']
