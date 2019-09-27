2a35413f1ca943ecbed8a5c4eb7b115fb97f8b43
nehaalhat <54702289+nehaalhat@users.noreply.github.com>
2019-09-27 18:11:53 +0530

    Implementation of rabbitmq (#5)

--- api/controller.py
+++ api/controller.py
@@ -3,13 +3,31 @@ import json
 from flask import jsonify
 from flask import make_response
 from flask import request
+from oslo_config import cfg
+import oslo_messaging as om
 from oslo_utils import uuidutils
 from webob import exc
 
+import config
 from common import exceptions
-from db import api
 from json_parser import JsonParser
 from migrate import app as application
+
+
+# Invoke "get_transport". This call will set default Configurations required
+# to Create Messaging Transport
+transport = om.get_transport(cfg.CONF)
+
+# Set/Override Configurations required to Create Messaging Transport
+cfg.CONF.set_override('transport_url', config.transport_url)
+
+# Create Messaging Transport
+transport = om.get_transport(cfg.CONF)
+
+# Create Target (Exchange, Topic and Server to listen on)
+target = om.Target(topic='testme')
+
+client = om.RPCClient(transport, target)
 
 
 class Controller():
@@ -17,117 +35,146 @@ class Controller():
 
     @application.route('/data', methods=['POST'])
     def post():
-        content = request.get_json()
-        with open('temp.json', 'w') as f:
-            json.dump(content, f)
-        json_obj = JsonParser("temp.json")
+        with application.app_context():
+            content = request.get_json()
+            with open('temp.json', 'w') as f:
+                json.dump(content, f)
+            json_obj = JsonParser("temp.json")
 
-        json_obj.validate_json_data_type(content)
-        json_obj.parse_json_data()
+            json_obj.validate_json_data_type(content)
+            json_obj.parse_json_data()
 
-        ml_uuid = uuidutils.generate_uuid()
-        url = content['DataURL']
-        ml_lib = content['variables']['mlLib']
-        is_form_cluster = content['variables']['isFormCluster']
-        storage_name = content['variables']['storageAccountName']
-        storage_type = content['variables']['storageAccountType']
+            ctxt = {}
+            ml_uuid = uuidutils.generate_uuid()
+            url = content['DataURL']
+            ml_lib = content['variables']['mlLib']
+            is_form_cluster = content['variables']['isFormCluster']
+            storage_name = content['variables']['storageAccountName']
+            storage_type = content['variables']['storageAccountType']
 
-        api.mldata_create(ml_uuid, url, ml_lib, is_form_cluster,
-                          storage_name, storage_type)
+            client.call(ctxt, 'mldata_create', uuid=ml_uuid, url=url,
+                        ml_lib=ml_lib, is_form_cluster=is_form_cluster,
+                        storage_name=storage_name, storage_type=storage_type)
 
-        resources = content['resource']
-        for res in resources:
-            res_uuid = uuidutils.generate_uuid()
-            resource_type = res.get('InfrastrctureType')
-            provider = res.get('provider')
-            if res.get('APIEndpoint'):
-                endpoint = res.get('APIEndpoint')
-            if res.get('PublicEndpoint'):
-                endpoint = res.get('PublicEndpoint')
-            username = res.get('username')
-            password = res.get('password')
-            token = res.get('token')
-            availability_zone = res.get('availabilityZone')
-            region = res.get('Region')
+            resources = content['resource']
+            for res in resources:
+                res_uuid = uuidutils.generate_uuid()
+                resource_type = res.get('InfrastrctureType')
+                provider = res.get('provider')
+                if res.get('APIEndpoint'):
+                    endpoint = res.get('APIEndpoint')
+                if res.get('PublicEndpoint'):
+                    endpoint = res.get('PublicEndpoint')
+                username = res.get('username')
+                password = res.get('password')
+                token = res.get('token')
+                availability_zone = res.get('availabilityZone')
+                region = res.get('Region')
 
-            api.resource_create(res_uuid, resource_type, provider, endpoint,
-                                username, password, token, availability_zone,
-                                region)
+                client.call(ctxt, 'resource_create', uuid=res_uuid,
+                            resource_type=resource_type, provider=provider,
+                            endpoint=endpoint, username=username,
+                            password=password, token=token,
+                            availability_zone=availability_zone,
+                            region=region)
 
-            if res['NodeList']:
-                for node in res['NodeList']:
-                    uuid = uuidutils.generate_uuid()
-                    resource_id = res_uuid
-                    node_ip = node.get('NodeIP')
-                    username = node.get('Username')
-                    password = node.get('Password')
-                    api.node_create(uuid, resource_id, node_ip,
-                                    username, password)
+                if res['NodeList']:
+                    for node in res['NodeList']:
+                        uuid = uuidutils.generate_uuid()
+                        resource_id = res_uuid
+                        node_ip = node.get('NodeIP')
+                        username = node.get('Username')
+                        password = node.get('Password')
 
-        return json.dumps(201, {'ContentType': 'application/json'})
+                        client.call(ctxt, 'node_create', uuid=uuid,
+                                    resource_id=resource_id, node_ip=node_ip,
+                                    username=username, password=password)
+
+            return json.dumps(201, {'ContentType': 'application/json'})
 
     @application.route("/resource/<id>", methods=["GET"])
     def get_resource(id):
-        try:
-            result = api.resource_detail(id)
-            return make_response(jsonify(result), 200)
-        except exceptions.ResourceNotFound:
-            msg = "Can not find requested resource: %s" % id
-            return exc.HTTPNotFound(explanation=msg)
+        with application.app_context():
+            try:
+                ctxt = {}
+                result = client.call(ctxt, 'resource_detail', id=id)
+                return result
+            except exceptions.ResourceNotFound:
+                msg = "Can not find requested resource: %s" % id
+                return exc.HTTPNotFound(explanation=msg)
 
     @application.route("/resource", methods=["GET"])
     def get_all_resources():
-        return jsonify(api.get_all_resources())
+        with application.app_context():
+            ctxt = {}
+            result = client.call(ctxt, 'get_all_resources')
+            return jsonify(result)
 
     @application.route("/resource/<id>", methods=["DELETE"])
     def resource_delete(id):
-        try:
-            api.resource_delete(id)
-            return {"status": 'success'}, 204
-        except exceptions.ResourceNotFound:
-            msg = "Can not find requested resource: %s" % id
-            return exc.HTTPNotFound(explanation=msg)
+        with application.app_context():
+            try:
+                ctxt = {}
+                client.cast(ctxt, 'resource_delete', id=id)
+                return {"status": 'success'}, 204
+            except exceptions.ResourceNotFound:
+                msg = "Can not find requested resource: %s" % id
+                return exc.HTTPNotFound(explanation=msg)
 
     @application.route("/mldata/<id>", methods=["GET"])
     def get_mldata(id):
-        try:
-            result = api.mldata_detail(id)
-            return make_response(jsonify(result), 200)
-        except exceptions.MLDataNotFound:
-            msg = "Can not find requested MLData: %s" % id
-            return exc.HTTPNotFound(explanation=msg)
+        with application.app_context():
+            try:
+                ctxt = {}
+                result = client.call(ctxt, 'mldata_detail', id=id)
+                return make_response(jsonify(result), 200)
+            except exceptions.MLDataNotFound:
+                msg = "Can not find requested MLData: %s" % id
+                return exc.HTTPNotFound(explanation=msg)
 
     @application.route("/mldata", methods=["GET"])
     def get_all_mldata():
-        return jsonify(api.get_all_mldata())
+        with application.app_context():
+            ctxt = {}
+            result = client.call(ctxt, 'get_all_mldata')
+            return jsonify(result)
 
     @application.route("/mldata/<id>", methods=["DELETE"])
     def mldata_delete(id):
-        try:
-            api.mldata_delete(id)
-            return {"status": 'success'}, 204
-        except exceptions.MLDataNotFound:
-            msg = "Can not find requested MLData: %s" % id
-            return exc.HTTPNotFound(explanation=msg)
+        with application.app_context():
+            try:
+                ctxt = {}
+                client.cast(ctxt, 'mldata_delete', id=id)
+                return {"status": 'success'}, 204
+            except exceptions.MLDataNotFound:
+                msg = "Can not find requested MLData: %s" % id
+                return exc.HTTPNotFound(explanation=msg)
 
     @application.route("/node/<id>", methods=["GET"])
     def get_node(id):
-        try:
-            result = api.node_detail(id)
-            return make_response(jsonify(result), 200)
-        except exceptions.NodeNotFound:
-            msg = "Can not find requested node: %s" % id
-            return exc.HTTPNotFound(explanation=msg)
+        with application.app_context():
+            try:
+                ctxt = {}
+                result = client.call(ctxt, 'node_detail', id=id)
+                return make_response(jsonify(result), 200)
+            except exceptions.NodeNotFound:
+                msg = "Can not find requested node: %s" % id
+                return exc.HTTPNotFound(explanation=msg)
 
     @application.route("/node", methods=["GET"])
     def get_all_nodes():
-        return jsonify(api.get_all_nodes())
+        with application.app_context():
+            ctxt = {}
+            result = client.call(ctxt, 'get_all_nodes')
+            return jsonify(result)
 
     @application.route("/node/<id>", methods=["DELETE"])
     def node_delete(id):
-        try:
-            api.node_delete(id)
-            return {"status": 'success'}, 204
-        except exceptions.NodeNotFound:
-            msg = "Can not find requested node: %s" % id
-            return exc.HTTPNotFound(explanation=msg)
+        with application.app_context():
+            try:
+                ctxt = {}
+                client.cast(ctxt, 'node_delete', id=id)
+                return {"status": 'success'}, 204
+            except exceptions.NodeNotFound:
+                msg = "Can not find requested node: %s" % id
+                return exc.HTTPNotFound(explanation=msg)
--- db/api.py
+++ db/api.py
@@ -1,6 +1,25 @@
+from oslo_config import cfg
+import oslo_messaging as om
+
+import config
 from common import exceptions
+from migrate import app
 import models
 from models import db
+
+
+# Invoke "get_transport". This call will set default Configurations required
+# to Create Messaging Transport
+transport = om.get_transport(cfg.CONF)
+
+# Set/Override Configurations required to Create Messaging Transport
+cfg.CONF.set_override('transport_url', config.transport_url)
+
+# Create Messaging Transport
+transport = om.get_transport(cfg.CONF)
+
+# Create Target (Exchange, Topic and Server to listen on)
+target = om.Target(topic='testme', server=config.rpc_server)
 
 
 def to_dict(func):
@@ -17,108 +36,124 @@ def to_dict(func):
     return decorator
 
 
-def resource_create(uuid, resource_type, provider, endpoint,
-                    username, password, token, availability_zone, region):
+# Create EndPoint
+class TestEndpoint(object):
 
-    new_res = models.Resource(uuid, resource_type, provider, endpoint,
-                              username, password, token, availability_zone,
-                              region)
+    def mldata_create(self, ctxt=None, uuid=None, url=None, ml_lib=None,
+                      is_form_cluster=None, storage_name=None,
+                      storage_type=None):
+        with app.app_context():
+            new_mldata = models.MLData(uuid, url, ml_lib, is_form_cluster,
+                                       storage_name, storage_type)
 
-    db.session.add(new_res)
-    db.session.commit()
+            db.session.add(new_mldata)
+            db.session.commit()
+
+    def resource_create(self, ctxt=None, uuid=None, resource_type=None,
+                        provider=None, endpoint=None,
+                        username=None, password=None, token=None,
+                        availability_zone=None, region=None):
+        with app.app_context():
+            new_res = models.Resource(uuid, resource_type, provider, endpoint,
+                                      username, password, token,
+                                      availability_zone, region)
+
+            db.session.add(new_res)
+            db.session.commit()
+
+    def node_create(self, ctxt=None, uuid=None, resource_id=None,
+                    node_ip=None, username=None, password=None):
+        with app.app_context():
+            new_node = models.Node(uuid, resource_id, node_ip, username,
+                                   password)
+
+            db.session.add(new_node)
+            db.session.commit()
+
+    @to_dict
+    def resource_detail(self, ctxt=None, id=None):
+        with app.app_context():
+            result = (models.Resource.query.filter(models.Resource.uuid == id)
+                      .outerjoin(models.Node)
+                      .one_or_none()
+                      )
+
+            if result is None:
+                raise exceptions.ResourceNotFound(uid=id)
+
+            return result
+
+    @to_dict
+    def get_all_resources(self, ctxt=None):
+        with app.app_context():
+            result = models.Resource.query.all()
+            return result
+
+    def resource_delete(self, ctxt=None, id=None):
+        with app.app_context():
+            resource = models.Resource.query.get(id)
+            if resource is None:
+                return exceptions.ResourceNotFound(uid=id)
+            db.session.delete(resource)
+            db.session.commit()
+
+    @to_dict
+    def get_all_mldata(self, ctxt=None):
+        with app.app_context():
+            result = models.MLData.query.all()
+            return result
+
+    @to_dict
+    def mldata_detail(self, ctxt=None, id=None):
+        with app.app_context():
+            result = models.MLData.query.get(id)
+
+            if result is None:
+                raise exceptions.MLDataNotFound(uid=id)
+
+            return result
+
+    def mldata_delete(self, ctxt=None, id=None):
+        with app.app_context():
+            data = models.MLData.query.get(id)
+            if data is None:
+                raise exceptions.MLDataNotFound(uid=id)
+
+            db.session.delete(data)
+            db.session.commit()
+
+    @to_dict
+    def get_all_nodes(self, ctxt=None):
+        with app.app_context():
+            result = models.Node.query.all()
+            return result
+
+    @to_dict
+    def node_detail(self, ctxt=None, id=None):
+        with app.app_context():
+            result = models.Node.query.get(id)
+
+            if result is None:
+                raise exceptions.NodeNotFound(uid=id)
+
+            return result
+
+    def node_delete(self, ctxt=None, id=None):
+        with app.app_context():
+            node = models.Node.query.get(id)
+
+            if node is None:
+                raise exceptions.NodeNotFound(uid=id)
+
+            db.session.delete(node)
+            db.session.commit()
 
 
-def mldata_create(uuid, url, ml_lib, is_form_cluster,
-                  storage_name, storage_type):
+# Create EndPoint List
+endpoints = [TestEndpoint(), ]
 
-    new_mldata = models.MLData(uuid, url, ml_lib, is_form_cluster,
-                               storage_name, storage_type)
+# Create RPC Server
+server = om.get_rpc_server(transport, target, endpoints)
 
-    db.session.add(new_mldata)
-    db.session.commit()
-
-
-def node_create(uuid, resource_id, node_ip, username, password):
-    new_node = models.Node(uuid, resource_id, node_ip, username,
-                           password)
-
-    db.session.add(new_node)
-    db.session.commit()
-
-
-@to_dict
-def resource_detail(id):
-    result = (models.Resource.query.filter(models.Resource.uuid == id)
-              .outerjoin(models.Node)
-              .one_or_none()
-              )
-
-    if result is None:
-        raise exceptions.ResourceNotFound(uid=id)
-
-    return result
-
-
-@to_dict
-def get_all_resources():
-    result = models.Resource.query.all()
-    return result
-
-
-def resource_delete(id):
-    resource = models.Resource.query.get(id)
-    if resource is None:
-        raise exceptions.ResourceNotFound(uid=id)
-    db.session.delete(resource)
-    db.session.commit()
-
-
-@to_dict
-def get_all_mldata():
-    result = models.MLData.query.all()
-    return result
-
-
-@to_dict
-def mldata_detail(id):
-    result = models.MLData.query.get(id)
-
-    if result is None:
-        raise exceptions.MLDataNotFound(uid=id)
-
-    return result
-
-
-def mldata_delete(id):
-    data = models.MLData.query.get(id)
-    if data is None:
-        raise exceptions.MLDataNotFound(uid=id)
-
-    db.session.delete(data)
-    db.session.commit()
-
-
-@to_dict
-def get_all_nodes():
-    result = models.Node.query.all()
-    return result
-
-
-@to_dict
-def node_detail(id):
-    result = models.Node.query.get(id)
-
-    if result is None:
-        raise exceptions.NodeNotFound(uid=id)
-
-    return result
-
-
-def node_delete(id):
-    node = models.Node.query.get(id)
-
-    if node is None:
-        raise exceptions.NodeNotFound(uid=id)
-
-    db.session.delete(node)
-    db.session.commit()
+# Start RPC Server
+server.start()
