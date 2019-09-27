5621998d602bf4a794c7fd5d3b093edcefdd9133
namrata <sitlani.namrata@yahoo.in>
2019-09-27 12:41:25 +0530

    device engine

---
+++ device_engine/manager.py
@@ -0,0 +1,11 @@
+
+"""
+This file include the RPC methods.
+"""
+
+class DeviceEngineManager(object):
+    def __init__(self, host, topic):
+        super(DeviceEngineManager, self).__init__(host, topic)
+
+
+
